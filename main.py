c30c854e9d5a4c8f3345a90d32973ce689ffb075
axl72 <axell.bernabel@unmsm.edu.pe>
2022-06-18 07:23:24 -0500

    Se corrigio error en las importaciones

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import tkinter as tk
-from view.board import boarConstructor
+from model.box import Box
 
-if __name__ == "__name__":
+boxesList = list()
+
+
+def boardConstructor(gameWindow):
+    """Funcion que nos permite constuir un tablero en la ventana de juego"""
+    for r in range(5):
+        for c in range(5):
+
+            # Se instancia el cuadrado. nota: Box recibe el componente padre, fila, columna, y un valor de 0(dorado) y 1(rojo oscuro)
+            box = Box(gameWindow, r, c, (r + c) % 2)
+            boxesList.append(box)
+            box.grid(row=r, column=c)
+
+
+if __name__ == "__main__":
     # Definicion de la ventana
     gameWindow = tk.Tk()
     gameWindow.resizable(0, 0)
