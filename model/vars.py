de20eef501d46be98b6810e8d2b019d5f5a596a2
axl72 <axell.bernabel@unmsm.edu.pe>
2022-06-18 04:35:38 -0500

    Definicion de variables globales

---
+++ model/vars.py
@@ -0,0 +1,27 @@
+BOX_NUMBERER_VAR = 0
+
+# Controla los colores de los cuadrados
+boxColourList = ['gold', 'darkred']
+
+# Variables de clase
+piecesList = []
+boxesList = []
+blank_box_widget_list = ['img/blankBox__Yo.png',
+                         b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00O\x00\x00\x00N\x08\x03\x00\x00\x00V\xb6\xbb\x11\x00\x00\x00\x01sRGB\x00\xae\xce\x1c\xe9\x00\x00\x00\x04gAMA\x00\x00\xb1\x8f\x0b\xfca\x05\x00\x00\x00\x03PLTE\x00\x00\x00\xa7z=\xda\x00\x00\x00\x01tRNS\x00@\xe6\xd8f\x00\x00\x00\tpHYs\x00\x00\x17\x11\x00\x00\x17\x11\x01\xca&\xf3?\x00\x00\x00\x1dIDATXG\xed\xc11\x01\x00\x00\x00\xc2\xa0\xf5Om\x06\x7f \x00\x00\x00\x00\x00\x80K\r\x18`\x00\x018\x118\xb7\x00\x00\x00\x00IEND\xaeB`\x82']
+
+ENTIRE_BOARD_MATRIX = []
+CHECK_MATE_COLOUR = ['grey', 'grey']
+clickBoxColourList = ['khaki', 'darkmagenta']
+ENTIRE_BOARD_MATRIX = []
+SIDE_OF_WHITE = 0
+HORIZONTAL_VERTICAL_ARRANGEMENT_VAR = [0, 1, 0, 1][SIDE_OF_WHITE]
+ENTIRE_BOARD_MATRIX = []
+PIECES_ALIVE = [[], []]
+COLOUR_OF_COMPUTER = 0
+COMPUTER_PROCESSING_STATUS = 0
+NUMBER_OF_HUMAN_PLAYERS = 1
+clickBoxColourList = ['khaki', 'darkmagenta']
+if NUMBER_OF_HUMAN_PLAYERS == 1:
+    MOVE_TIME_INTERVAL = 1000
+else:
+    MOVE_TIME_INTERVAL = 10
