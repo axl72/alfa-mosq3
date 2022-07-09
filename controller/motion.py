42774df6f43c42b595413ca64e6c813bd96fff44
axl72 <axell.bernabel@unmsm.edu.pe>
2022-07-09 04:14:01 -0500

    Se adapto al nuevo módulo

--- controller/motion.py
+++ controller/motion.py
@@ -1,53 +1,8 @@
+from controller.util import *
+
 clickBoxColourList = {'gold': 'khaki', 'darkred': 'darkmagenta'}
 boxColourList = ('gold', 'darkred')
 PIECE_CLICKED = (-1, -1)
-
-
-def suma_cartesiana(tupla1, tupla2):
-    "Esta funcion nos permite obtener la suma cartesiana dada dos tuplas"
-    return tuple([a + b for a, b in zip(tupla1, tupla2)])
-
-
-def obtenerCordenadasFrontales(cordendas):
-    "Esta fucion nos permite obtener las cordenadas frontales y laterales dada una cordenada"
-    movimientos = {"izquierda": (-1, 0), "arriba": (0, -1),
-                   "derecha": (1, 0), "abajo": (0, 1)}
-
-    resultado = list()
-    for i in movimientos.values():
-        resultado.append(suma_cartesiana(cordendas, i))
-    return resultado
-
-
-def obtenerCordenadasPosibles(boxesMatrix, piecesMatrix, juega_mosquetero, cordenadas):
-    """Esta funcion recibe la cordenada del tablero seleccionada para iniciar una jugada, retornar una lista de cordenadas posibles a donde moverse en base al tipo de ficha"""
-
-    size = len(boxesMatrix) - 1
-
-    if size <= 0:
-        raise Exception
-
-    cordenadasPosibles = obtenerCordenadasFrontales(cordenadas)
-    cordenadasFrontales = list()
-
-    # Con este bucle se eliminan las cordenadas negativas o absurdas
-    for index, z in enumerate(cordenadasPosibles):
-        x, y = z
-        if not((x > size or y > size) or (x < 0 or y < 0)):
-            cordenadasFrontales.append(z)
-
-    # Con este bucle se eliminan las jugadas que carecen de lógica como que un mosquetero inte comer a otro
-    cordenadasValidadas = list()
-    for z in cordenadasFrontales:
-        x, y = z
-        codigo_pieza = boxesMatrix[x][y].piece_contained
-        if juega_mosquetero[0]:
-            if codigo_pieza == 0:
-                cordenadasValidadas.append(z)
-        else:
-            if codigo_pieza == -1:
-                cordenadasValidadas.append(z)
-    return cordenadasValidadas
 
 
 def pintarTablero(boxesMatrix, piecesMatrix, cordenadas, color=None):
@@ -68,7 +23,7 @@ def pintarTablero(boxesMatrix, piecesMatrix, cordenadas, color=None):
             boxesMatrix[x][y].config(bg=color)
 
 
-def siguienteTurno(boxesMatrix, piecesMatrix, juega_mosquetero, cordenadas, preseleccion):
+def siguienteTurno(tablero, boxesMatrix, piecesMatrix, juega_mosquetero, cordenadas, preseleccion):
     # Si ya hemos elegido la ficha a mover y luego elegimos un movimiento valido
     global PIECE_CLICKED
     if cordenadas in preseleccion:
@@ -76,8 +31,10 @@ def siguienteTurno(boxesMatrix, piecesMatrix, juega_mosquetero, cordenadas, pres
         cordenadas
         if juega_mosquetero[0]:
             piecesMatrix[x][y].comerPieza(cordenadas)
+            tablero.mover_pieza(PIECE_CLICKED, cordenadas, juega_mosquetero[0])
         else:
             piecesMatrix[x][y].moverPieza(cordenadas)
+            tablero.mover_pieza(PIECE_CLICKED, cordenadas, juega_mosquetero[0])
         pintarTablero(boxesMatrix, piecesMatrix, preseleccion)
 
         # Esto se puede optimizar
@@ -87,11 +44,11 @@ def siguienteTurno(boxesMatrix, piecesMatrix, juega_mosquetero, cordenadas, pres
         # Dado el error 'referenced before assignment' la linea queda así
         # Debería ser de esta forma 'return not juega_mosquetero'
         juega_mosquetero[0] = not juega_mosquetero[0]
+
         return
 
     cordenadas_posibles = obtenerCordenadasPosibles(
-        boxesMatrix, piecesMatrix, juega_mosquetero, cordenadas)
-
+        tablero.estado, cordenadas, juega_mosquetero[0])
     if cordenadas_posibles == []:
         print("No hay jugadas posibles para esta ficha")
         return
@@ -101,7 +58,6 @@ def siguienteTurno(boxesMatrix, piecesMatrix, juega_mosquetero, cordenadas, pres
 
     if preseleccion != []:
         pintarTablero(boxesMatrix, piecesMatrix, preseleccion)
-    # else:
 
     x, y = cordenadas_posibles[0]
     color = clickBoxColourList[boxesMatrix[x][y].colour]
